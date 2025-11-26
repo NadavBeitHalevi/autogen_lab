9bb2bc536fb6346eb3137554ba97fdf91789236f
nadav.beithalevi <nadav.beithalevi@gmail.com>
2025-11-26 21:33:02 +0200

    created the first autohen example

--- src/main.py
+++ src/main.py
@@ -1,15 +1,40 @@
 import dotenv
-import os
+import asyncio
+from autogen_ext.models.openai import OpenAIChatCompletionClient
+from autogen_agentchat.agents import AssistantAgent
+from autogen_agentchat.messages import TextMessage
+from autogen_core import CancellationToken
 
-dotenv.load_dotenv(dotenv_path=".env.example", override=True)
+dotenv.load_dotenv(override=True)
 
-def main():
-    print(f"Hello, AutoGen Lab!")
-    openai_api_key = os.getenv("OPENAI_API_KEY")
-    if openai_api_key:
-        print("OpenAI API Key loaded successfully.")
+async def get_weather(city:str) -> str:
+    '''Get weather for a given city'''
+    return f"The weather in {city} is sunny with a high of 75°F."
+
+
+async def main() -> None:
+    """Initialize and run the AutoGen Lab application.
+    
+    Sets up the OpenAI model client for agent interactions.
+    """
+    message = TextMessage(content="Hello, AutoGen Lab!, I'd like to go to Tel-Aviv",
+                          source="user")
+
+    print(f"Wlcome to AutoGen Lab :)")
+    model_client: OpenAIChatCompletionClient = OpenAIChatCompletionClient(model="gpt-4")
+    
+    agent = AssistantAgent(name="WeatherBot", 
+                           model_client=model_client,
+                           system_message="You are a helpful assistant for an airline. You give short, humorous answers.",
+                           model_client_stream=True)
+    
+    response = await agent.on_messages([message], cancellation_token=CancellationToken())
+    # print(f'Full response: {response}')
+
+    if isinstance(response.chat_message, TextMessage):
+        print(f"response content: {response.chat_message.content}")
     else:
-        print("OpenAI API Key not found. Please set it in the .env file.")
+        print(f"response: {response.chat_message}")
     
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
